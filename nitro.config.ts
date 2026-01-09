480dfe043f2949e9521b0522c2478a135c93528c
cowlai <support@druidui.com>
2026-01-09 16:03:30 +0000

    Fix run

--- nitro.config.ts
+++ nitro.config.ts
@@ -2,4 +2,7 @@ import { defineNitroConfig } from 'nitro/config'
 
 export default defineNitroConfig({
   preset: 'node-server',
+  externals: {
+    traceInclude: ['react-dom', 'react-dom/server'],
+  },
 })
