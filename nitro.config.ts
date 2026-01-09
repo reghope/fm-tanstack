542c50fc23bbccf260ad77dfbd8b83d5e58650e4
cowlai <support@druidui.com>
2026-01-09 16:11:52 +0000

    Fix bun preset in defineconfig

--- nitro.config.ts
+++ nitro.config.ts
@@ -2,6 +2,7 @@ import { defineNitroConfig } from 'nitro/config'
 
 export default defineNitroConfig({
   preset: 'node-server',
+  exportConditions: ['bun', 'node', 'import', 'default'],
   externals: {
     traceInclude: ['react-dom', 'react-dom/server'],
   },
